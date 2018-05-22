0432951427013da61b82b9ceee0b82f7a348903d
Nathan Smith <nathan@praisetopia.org>
2018-05-22 08:05:20 -0500

    2nd commit

--- extension.js
+++ extension.js
@@ -1,9 +1,18 @@
 'use strict'
 
+const vscode = require('vscode');
+const config = vscode.workspace.getConfiguration('brewdown');
+
 function activate(context) {
+    const options = {
+        "pageSize": config.get('pageSize')
+    };
+    if(config.get('twoCol')) {
+        options.style = 'two-col';
+    }
     return {
         extendMarkdownIt(md) {
-            return md.use(require('brewdown'));
+            return md.use(require('brewdown'), options);
         }
     };
 }
